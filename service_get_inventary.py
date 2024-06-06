d146b2600cedb86604f9ed131b65b9bdb1fafd43
METROH <alejandrodesss@gmail.com>
2024-06-06 00:28:29 -0400

    aaaaaa

--- service_get_inventary.py
+++ service_get_inventary.py
@@ -1,15 +1,134 @@
 # Ejemplo de servicio
 from soa_service import Soa_Service
 from util.list_of_services import service
+import psycopg2
+import json
+from datetime import date, datetime
+from decimal import Decimal
+POSTGRES_HOST = "172.17.0.2"  # METER IP DE SU DOCKER CONTAINER
+POSTGRES_PORT = "5432"        # PostgreSQL port debería ser este si no lo cambiaron el default
+POSTGRES_DB = "manga_db"
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "1234"
+
+def custom_serializer(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    if isinstance(obj, Decimal):
+        return float(obj)
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+
+def Get_Book(book_id):
+    try:
+        # Connect to PostgreSQL
+        conn = psycopg2.connect(
+            host=POSTGRES_HOST,
+            port=POSTGRES_PORT,
+            database=POSTGRES_DB,
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD
+        )
+        # Create a cursor object
+        cur = conn.cursor()
+
+        # Execute SQL query to search for user
+        query = """--sql
+         SELECT * FROM manga WHERE id = %s;
+        """
+        cur.execute(query, (book_id))
+
+        # Fetch one record
+        book_info = cur.fetchone()
+
+        # Close cursor and connection
+        cur.close()
+        conn.close()
+
+        if book_info is None:
+            return False
+        
+        attributes = [desc[0] for desc in cur.description]
+
+        # Formatear el resultado como un diccionario
+        book_info = dict(zip(attributes, book_info))
+        
+        book_info = json.dumps(book_info, indent=2, default=custom_serializer, ensure_ascii=False)
+
+        
+        return book_info
+
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return False
+
+def Get_All():
+    try:
+        # Connect to PostgreSQL
+        conn = psycopg2.connect(
+            host=POSTGRES_HOST,
+            port=POSTGRES_PORT,
+            database=POSTGRES_DB,
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD
+        )
+        # Create a cursor object
+        cur = conn.cursor()
+
+        # Execute SQL query to search for user
+        query = """--sql
+         SELECT * FROM manga;
+        """
+        cur.execute(query)
+
+        # Fetch one record
+        book_info = cur.fetchall()
+
+        # Close cursor and connection
+        cur.close()
+        conn.close()
+
+        if book_info is None:
+            return False
+        
+        attributes = [desc[0] for desc in cur.description]
+
+        # Formatear el resultado como un diccionario
+        book_info = [dict(zip(attributes, row)) for row in book_info]
+        
+        book_info = json.dumps(book_info, indent=2, default=custom_serializer, ensure_ascii=False)
+
+        
+        return book_info
+
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return False
+
 
 class Get_Inventary(Soa_Service):
     # ACA SE HACE LA MAGIA XD
     def process_data(self, request):
         global service_name
         
-        response = "XDDDDDD" + request + "XDDDDDD"
+        if len(request.split(" ")) != 1:
+            response = "Error de formato"
+            return service_name, response
         
-        return service_name, response
+        if request == "all":
+            book_info = Get_All()
+        else:
+            book_info = Get_Book(request)
+        
+        
+        if book_info is False:
+            response = "Ocurrió un error"
+            return service_name, response
+        
+        
+        return service_name, book_info
 
 
 
