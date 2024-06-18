8a05f360d67d8dbde3e760e2e6f7e84c0871959f
dielo17 <diego.lopez1@mail.udp.cl>
2024-06-17 20:50:48 -0400

    a

--- service_notification.py
+++ service_notification.py
@@ -117,7 +117,7 @@ class CustomService(Soa_Service):
             return service_name, "No se encontró el correo del usuario"
 
         remitente = "correodetareas11.9@gmail.com"
-        contraseña = os.getenv("PASSWORD")
+        contraseña = "otxm trlz hnvd wlxk"
         cuerpo = ""
 
         if asunto == "Mangas estrenados este mes": # Asunto 1
